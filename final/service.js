5cc3ad86120257a8afd1eabf76970c71daff1d4d
Antuan01 <joseitox_4@hotmail.com>
2021-04-13 13:43:25 -0400

    updates

--- final/service.js
+++ final/service.js
@@ -3,13 +3,16 @@
 self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-    console.log(event)
+    console.log(`[Service Worker] Push had this: "${event.data}"`);
+    console.log(event);
 
-    const title = 'Pendiente Perrocaliente';
+    const title = 'TuBotones';
+
     const options = {
       body: event.data.text(),
       icon: 'images/icon.png',
-      badge: 'images/badge.png'
+      badge: 'images/badge.png',
+      image: "https://staging.api.tubotones.com/storage/voucher/1617813685.jpg",
     };
   
     event.waitUntil(self.registration.showNotification(title, options));
